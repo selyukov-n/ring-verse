1a72bada00d60a7f505ad258fb3b0ab0a0ed15fa
Nickolay Selyukov <nselyukov@gmail.com>
2020-11-22 23:28:22 +0500

    fix some language codes

--- src/languages/encodings.ts
+++ src/languages/encodings.ts
@@ -2,8 +2,8 @@ import { groups as g } from "./names";
 import { group } from "./utils";
 
 export const encodings = group(g.encoding,
-  group(g._en, "e.c", "e.pig", "e.eli", "e.bra", "e.ges", "e.mor", "e.hex", "e.sou", "e.dmsnd", "e.nysiis"),
+  group(g._en, "e.c", "e.pig", "e.eli", "e.braille", "e.gest", "e.morse", "e.hex", "e.sndex", "e.dmsnd", "e.nysiis"),
   group(g._fr, "e.ver"),
-  group(g._he, "e.g", "e.nut"),
+  group(g._he, "e.g", "e.nuts"),
 );
 export type EncodingLang = (typeof encodings)["id"];
