ef9270cefb05972afc268b573a6d75459454da85
Nickolay Selyukov <nselyukov@gmail.com>
2020-11-23 00:00:04 +0500

    collapsible langtree

--- src/LangTree.tsx
+++ src/LangTree.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Link } from "react-router-dom";
 import { arda, conlangs, encodings, jokes, languages, names, LangGroupItem } from "./languages";
 import { Lines } from "./Lines";
@@ -7,17 +7,24 @@ import original from "./data/main";
 type Props<T extends string> = {
   item: LangGroupItem<T>;
   names: Record<T, string>;
+  defaultOpen?: boolean;
 };
 
-const Node = <T extends string>({ item, names }: Props<T>): JSX.Element => {
-  const children = item.type === "group" && <ul>
+const Node = <T extends string>({ item, names, defaultOpen = false }: Props<T>): JSX.Element => {
+  const [open, toggle] = useState(defaultOpen);
+  const onClick = useCallback(() => toggle(!open), [open]);
+
+  if (item.type !== "group")
+    return <li>
+      <Link to={'/lang/' + item.id}>{names[item.id]}</Link>
+    </li>;
+
+  const className = open ? "exp expanded" : "exp collapsed";
+  const children = item.type === "group" && open && <ul className="content">
     {item.items.map((it, i) => <Node item={it} names={names} key={i} />)}
   </ul>;
-  return <li>
-    {item.type === "group"
-      ? item.name
-      : <Link to={'/lang/' + item.id}>{names[item.id]}</Link>}
-
+  return <li className={className}>
+    <span onClick={onClick}>{item.name}</span>
     {children}
   </li>
 };
@@ -27,9 +34,9 @@ const LangTree = () => {
     <Lines text={original.text} footer={"© " + original.author} className={"original"} />
 
     <ol>
-      <Node item={arda} names={names.arda} />
-      <Node item={languages} names={names.languages} />
-      <Node item={conlangs} names={names.conlangs} />
+      <Node item={arda} names={names.arda} defaultOpen />
+      <Node item={languages} names={names.languages} defaultOpen />
+      <Node item={conlangs} names={names.conlangs} defaultOpen />
       <Node item={encodings} names={names.encodings} />
       <Node item={jokes} names={names.jokes} />
     </ol>
