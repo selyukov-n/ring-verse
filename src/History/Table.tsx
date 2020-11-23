62a470aa58a485036130fd6769426e13ea6e3282
Nickolay Selyukov <nselyukov@gmail.com>
2020-11-24 03:24:41 +0500

    history: output langs and sources

--- src/History/Table.tsx
+++ src/History/Table.tsx
@@ -1,21 +1,40 @@
 import React from "react";
 import { Table as T } from "react-bootstrap";
+import { useDataContext } from "../context";
 import hist from "../dataHistory";
 import { formatDate } from "./utils";
 
 export const Table = () => {
+  const { num } = useDataContext();
+
   let total = 0;
+  const langs = new Set<string>();
   const rows = hist.map(h => {
     const date = h.date ? formatDate(h.date) : "?";
-    total += h.count;
+
+    const iter = {
+      langs: new Set<string>(),
+      sources: new Set<string>(),
+    };
+
+    for(let t = 1; t <= h.count; t++, total++) {
+      const { language: l, source } = num[total + 1];
+      if (!langs.has(l)) {
+        langs.add(l);
+        iter.langs.add(l);
+      }
+      if (source?.main)
+        iter.sources.add(source.main);
+    }
+
     return <tr key={h.ver}>
       <td>{h.ver}</td>
       <td>{date}</td>
       <td>{h.count}</td>
       <td>{total}</td>
-      <td>TODO get new langs</td>
-      <td>TODO get sources</td>
-      <td>TODO count distinct langs (total)</td>
+      <td>{Array.from(iter.langs).join(", ")}</td>
+      <td>{Array.from(iter.sources).join(", ")}</td>
+      <td>TODO total</td>
     </tr>;
   });
 
