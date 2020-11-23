e82250b3c9b536f1f6522eb00a19bb23e284cf7f
Nickolay Selyukov <nselyukov@gmail.com>
2020-11-24 00:35:28 +0500

    add check index

--- src/data/utils.ts
+++ src/data/utils.ts
@@ -13,6 +13,7 @@ export const fromLotR = (name?: string) => "из перевода ВК" + (name 
 export const forEach = <T>(input: Record<number, T>, func: (t: T, num: number) => any) => {
   Object.keys(input).forEach(k => {
     const num = parseInt(k);
+    if (!num) console.error('cannot parse number from ' + k);
     func(input[num], num);
   });
 };
