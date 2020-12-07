0c11b9b3b8a35a825b058ca511d7b7c2ffa1ba03
Nickolay Selyukov <nselyukov@gmail.com>
2020-12-08 00:28:11 +0500

    add type: text-large

--- src/data/lang/viet.ts
+++ src/data/lang/viet.ts
@@ -4,7 +4,10 @@ export const viet = make({ language: "vie" }, {
   506: {
     input: "1.1",
     author: [],
-    content: `Ba Nhẫn cho các Tiên Chúa sống dưới gầm trời xanh,
+    content: [
+      {
+        type: "text-large",
+        text: `Ba Nhẫn cho các Tiên Chúa sống dưới gầm trời xanh,
 Bảy cho các Chúa Người Lùn dưới sảnh đá bất diệt,
 Chín cho Con Người Phàm Trần định mệnh dành phải chết
 Một cho Chúa Tể Hắc Ám ngự cao trên ngai đen
@@ -12,5 +15,7 @@ Một cho Chúa Tể Hắc Ám ngự cao trên ngai đen
 Một Nhẫn Chúa thống trị tất cả, một Nhẫn Chúa tìm ra hết,
 Một Nhẫn Chúa tập hợp tất cả, trong bóng tối trói buộc hết
 Ở Vùng Đất Đen Mordor nơi trú ngụ Bóng Đêm.`
+      }
+    ]
   },
 });
