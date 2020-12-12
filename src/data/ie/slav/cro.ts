339e8c5bdb01bd4f51e5dbc74e93a60e0d2d07da
Nickolay Selyukov <nselyukov@gmail.com>
2020-12-12 22:00:18 +0500

    fix

--- src/data/ie/slav/cro.ts
+++ src/data/ie/slav/cro.ts
@@ -6,7 +6,9 @@ const cro = make({ language: "cro", input: "0.2.0" }, {
     name: fromLotR(),
     book: book("S", 114, "and C-1, p. 114"),
     author: "Berislav Lopac",
-    content: `Prstena Tri za vilin-kralje za koje zvijezde siju,
+    content: [
+      {
+        text: `Prstena Tri za vilin-kralje za koje zvijezde siju,
 I Sedam za patuljke-vladare kamenih dvora moċi;
 I Devet za ljude, usuda kletog što zarana mriju,
 Al Prsten Jedan za Cara Mraka na prijestolju noczi
@@ -14,12 +16,26 @@ U zemlji Mordor gdje sjene se kriju.
 Prsten Jedan da zavlada svima, Prsten Jedan što trażi i seże
 Prsten Jedan da spoji se s njima i u tami ih sveże
 U zemlji Mordor gdje sjene se kriju.`
+      },
+      {
+        text: `Прстена Три за вилин-краље за које звијезде сију
+И седам за патуљке-владаре камених двора моћи,
+И девет за људе усуда клетог што рано мрију,
+Ал' прстен један за Цара Мрака на пријестољу ноћи
+У Земљи Мордор, где сјене се крију.
+Прстен један да завлада свима, Прстен један што тражи и сеже,
+Прстен један да се спои с њима и у тами их свеже
+У Земљи Мордор, где сјене се крију.`
+      }
+    ]
   },
   68: {
     name: fromLotR('"Stilos" edition'),
     book: book("SC", 113),
     author: "Zlatko Crnković",
-    content: `Tri Prstena za prste Kraljeva vilin-vrste pod nebesima što sjaju,
+    content: [
+      {
+        text: `Tri Prstena za prste Kraljeva vilin-vrste pod nebesima što sjaju,
 Sedam za vladare Patuljaka u dvoru njihovom kamnom,
 Devet za Smrtne Ljude koje smrt čeka na kraju,
 Jedan za Mračnog Gospodara na njegovom prestolu tamnom
@@ -27,6 +43,18 @@ U Zemlji Mordor gde Senke traju.
 Jedan Prsten da svima gospodari, jedan za svima seže,
 Jedan Prsten da sve okupi i u tami ih sveže
 U Zemlji Mordor gde Senke traju.`
+      },
+      {
+        text: `Три Прстена за прсте Краљева вилин-врсте под небесима што сјају,
+Седам за владаре Патуљака у двору њиховом камном,
+Девет за смртне људе које смрт чека на крају,
+Један за мрачног Господара на његовом престолу тамном
+У Земљи Мордор, где сенке трају.
+Један Прстен да свима господари, један за свима сеже,
+Један Прстен да све окупи и у тами их свеже
+У Земљи Мордор, где сенке трају.`
+      }
+    ]
   },
 });
 
@@ -59,6 +87,7 @@ U zemlji Mordor gde senke leze`
   },
   467: {
     input: "1.0.1",
+    variant: "хорватский",
     author: [],
     book: ["C-2", 115],
     content: `Tri prstena za vilen-kraljeve ispod neba,
