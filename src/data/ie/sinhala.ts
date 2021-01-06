b68f150dcc620d814c278ace468b28d69804e58f
Nickolay Selyukov <nselyukov@gmail.com>
2021-01-07 00:07:31 +0500

    add (fixed) Sinhala script + transliteration

--- src/data/ie/sinhala.ts
+++ src/data/ie/sinhala.ts
@@ -5,9 +5,34 @@ export const sinhala = make({ language: "sin" }, {
   526: {
     input: "2.2.0",
     author: [],
-    content: [{
-      type: "image",
-      image: pic526,
-    }]
+    content: [
+      {
+        type: "image",
+        image: pic526,
+      },
+      {
+        type: "text-large",
+        text: `නිලඹර පාමුලෙහි, එල්ෆ්‌ නිරිඳුන්‌ වෙත ත්‍රිත්වයකි
+ශෛලමය මාළිගා අරා සිටි කුරු නායකයන්‌ කෙරෙහි සතකි
+මරණයම හිමි, මරණිය නරයන්‌ වෙත ද නවයකි
+නමුදු
+සෙවණැලි නිදන මොඩෝර්‌ දේශයේ
+එකම එක තනි මුදුවකි,
+සියල්ල පාලනයට තනි මුදුවකි,
+සියල්ල සොයාගැනීමට තනි මුදුවකි
+සියල්ල එකමිටින්‌ අඳුරේ බැඳ දමනු වස්‌ එකම එක, තනි මුදුවකි`
+      },
+      {
+        text: `nilam̆bara pāmulehi, elf nirin̆dun veta tritvayaki
+śailamaya māḷigā arā siṭi kuru nāyakayan kerehi sataki
+maraṇayama himi, maraṇiya narayan veta da navayaki
+namudu
+sevaṇæli nidana moḍōr dēśayē
+ekama eka tani muduvaki,
+siyalla pālanayaṭa tani muduvaki,
+siyalla soyāgænīmaṭa tani muduvaki
+siyalla ekamiṭin an̆durē bæn̆da damanu vas ekama eka, tani muduvaki`
+      },
+    ]
   },
 });
