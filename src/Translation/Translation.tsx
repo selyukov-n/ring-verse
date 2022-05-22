026755b6100891793767896db8d7435ba583c75a
nselyukov <nselyukov@gmail.com>
2022-05-22 23:03:29 +0500

    add route for /show/last

--- src/Translation/Translation.tsx
+++ src/Translation/Translation.tsx
@@ -4,10 +4,14 @@ import { Alert } from "react-bootstrap";
 import { useDataContext } from "../context";
 import { TransItem } from "./TransItem";
 
-export const Translation: FC<RouteComponentProps<{ id: string }>> = props => {
+export const Translation: FC<RouteComponentProps<{ id: string }>> = ({ match: { params } }) => {
   const { num } = useDataContext();
-  const id = parseInt(props.match.params.id);
-  const item = num[id];
+
+  const keys = params.id === "last"
+      ? Object.keys(num)
+      : [params.id];
+  const id = Math.max(...keys.map(i => parseInt(i)).filter(Number.isFinite));
+  const item = id && num[id];
 
   return item
     ? <TransItem item={{ ...item, index: id }} linkToLang />
